c42f1d63a040784b13230a729b73a88aa43ac89a
talbert1209 <talbert1209@gmail.com>
2018-12-07 11:47:00 -0600

    Completed app.

--- SimpleTextEditor/Form1.cs
+++ SimpleTextEditor/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,16 +18,31 @@ namespace SimpleTextEditor
         public Form1()
         {
             InitializeComponent();
+            saveFileDialog1.InitialDirectory = @"C:\Users\talbert\Desktop\Test Streams";
+            openFileDialog1.InitialDirectory = @"C:\Users\talbert\Desktop\Test Streams";
+            saveFileDialog1.Filter = @"Text Files (*.txt)|*.txt";
+            openFileDialog1.Filter = @"Text Files (*.txt)|*.txt";
+            saveFileDialog1.Title = @"Save This Shit, Bro";
+            openFileDialog1.Title = @"Open This Shit, Bro";
         }
 
         private void openButton_Click(object sender, EventArgs e)
         {
-
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                _name = openFileDialog1.FileName;
+                textBox1.Clear();
+                textBox1.Text = File.ReadAllText(_name);
+            }
         }
 
         private void saveButton_Click(object sender, EventArgs e)
         {
-
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                _name = saveFileDialog1.FileName;
+                File.WriteAllText(_name, textBox1.Text);
+            }
         }
     }
 }
